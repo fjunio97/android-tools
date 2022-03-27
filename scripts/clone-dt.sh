ee18bef1c13300d88017b0866b1aa6aaabe2492d
Junio <flaviojunio1997@gmail.com>
2022-03-27 15:40:08 -0300

    Add files via upload

---
+++ scripts/clone-dt.sh
@@ -0,0 +1,13 @@
+#
+
+# device tree
+git clone git@github.com:fjunio97/device_xiaomi_ginkgo.git device/xiaomi/ginkgo
+
+# kernel
+git clone git@github.com:fjunio97/kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo
+
+# vendor
+git clone git@github.com:fjunio97/vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo
+
+# miui camera
+git clone git@github.com:fjunio97/vendor_miuicamera.git vendor/miuicamera
