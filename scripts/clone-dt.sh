0a6ee6002739ccb6d69fe4bc36a72e370dac21c4
Junio <flaviojunio1997@gmail.com>
2023-04-29 20:20:00 -0300

    Update clone-dt.sh

--- scripts/clone-dt.sh
+++ scripts/clone-dt.sh
@@ -1,13 +1,13 @@
 #
 
 # device tree
-git clone git@github.com:fjunio97/device_xiaomi_ginkgo.git device/xiaomi/ginkgo
+git clone git@github.com:juniosys/device_xiaomi_ginkgo.git device/xiaomi/ginkgo
 
 # kernel
-git clone git@github.com:fjunio97/kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo
+git clone git@github.com:juniosys/kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo
 
 # vendor
-git clone git@github.com:fjunio97/vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo
+git clone git@github.com:juniosys/vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo
 
 # miui camera
-git clone git@github.com:fjunio97/vendor_miuicamera.git vendor/miuicamera
+git clone git@github.com:juniosys/vendor_miuicamera.git vendor/miuicamera
